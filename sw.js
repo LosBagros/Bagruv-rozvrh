9e93d4e0149a8e62c7f188fcbec76b5af431f018
LosBagros <wifi5@centrum.cz>
2022-12-12 14:09:14 +0100

    vubec nevim co delam

--- sw.js
+++ sw.js
@@ -1,47 +1,27 @@
-// This is the "Offline page" service worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "index.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+self.addEventListener("install", function () {
+  console.log("Install!");
+});
+self.addEventListener("activate", (event) => {
+  console.log("Activate!");
+});
+self.addEventListener("fetch", function (event) {
+  console.log("Fetch!", event.request);
 });
 
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
-
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
+navigator.serviceWorker.ready.then(function (registration) {
+  if (!registration.pushManager) {
+    alert("No push notifications support.");
+    return false;
   }
+  //To subscribe `push notification` from push manager
+  registration.pushManager
+    .subscribe({
+      userVisibleOnly: true, //Always show notification when received
+    })
+    .then(function (subscription) {
+      console.log("Subscribed.");
+    })
+    .catch(function (error) {
+      console.log("Subscription error: ", error);
+    });
 });
