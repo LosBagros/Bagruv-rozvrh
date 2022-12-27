ff9e9804070fa9ff9aa79320ed37b99ccd1c0824
Bagros <45005533+LosBagros@users.noreply.github.com>
2022-12-27 12:47:24 +0100

    Update bakalari_data_importer.js

--- cloudflare_workers/bakalari_data_importer.js
+++ cloudflare_workers/bakalari_data_importer.js
@@ -1,5 +1,16 @@
 // Cloudflare Worker 1
 // Description: Imports data from Bakalari API to Cloudflare KV
+let success = true;
+addEventListener('scheduled', event => {
+  event.waitUntil(
+    handleSchedule()
+  )
+})
+
+async function handleSchedule() {
+  await getData();
+  console.log(success);
+}
 async function getBearerToken() {
     const reqDate = Math.floor(Date.now() / 1000);
     const options = {
@@ -64,7 +75,13 @@ async function getTimetable(bearerToken) {
 addEventListener("fetch", (event) => {
 
     event.respondWith(async function () {
-        const reqDate = Math.floor(Date.now() / 1000);
+        await getData();
+        return new Response(success);
+    }());
+});
+
+async function getData(){
+    const reqDate = Math.floor(Date.now() / 1000);
         let bearerToken = await BKDB.get("token", "json");
         if (!bearerToken) { bearerToken = await getBearerToken() }
         if (bearerToken.time + bearerToken.expires_in + 960 < reqDate) { bearerToken = await refreshBearerToken(bearerToken) }
@@ -75,9 +92,7 @@ addEventListener("fetch", (event) => {
         let hours = timetableString.search("Hours");
         if (hours != -1) {
             await BKDB.put("timetable", JSON.stringify(timetable));
-            return new Response("Data saved");
         } else {
-            return new Response("Data not saved");
+            success = false;
         }
-    }());
-});
+}
